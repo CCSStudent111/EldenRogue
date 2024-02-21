27c1b574de74d55267aae33e91906bfb29378d3c
lilraffyel <rafael_calalang@dlsu.edu.ph>
2024-02-21 20:20:40 +0800

    Create CharacterCreation.java
    TODO:
    - Create separate methods for each switch case statement
    - Logic fixes
    
    Will fix when im done studying for my other tests

---
+++ CharacterCreation.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+public class CharacterCreation{
+    private static String playerName;
+    private static String playerClass;
+    public static void main (String[] args){
+        Scanner sName = new Scanner(System.in);
+        int nInput;
+
+        do{
+            System.out.println("Character Creation\n\n");
+            System.out.println("1. Input Name");
+            System.out.println("2. Select Job Class");
+            System.out.println("3. Confirm");
+            System.out.println("4. Back\n");
+            nInput = scanner.nextInt();
+            if(nInput < 1 || nInput > 4){
+                System.out.println("Invalid Input!");
+            }
+
+            switch(nInput){
+            case 1: 
+                inputname(playerName);
+                break;
+            case 2:
+                // Getter Methods + if statement? //
+                break;
+            case 3:
+                // add if statements for name and job class //
+                System.out.println("Are you sure you want to proceed?\n");
+                System.out.println("1. Yes");
+                System.out.println("2. No");
+                nInput = scanner.nextInt();
+    
+                if(nInput == 1){
+                    //Proceed to Game Lobby//
+                    } else if(nInput == 2){ 
+                    break;
+                    } else{
+                    System.out.println("Invalid Input!");
+                    break;
+                    } 
+                }
+        }while(nInput != 4);
+    }
+
+private static String inputName(Scanner sName){
+    do{
+        System.out.println("Enter your username (25 character limit):\n");
+        playerName = sName.nextLine();
+            if(playerName.length() != 0 && playerName.length() <= 25){
+                System.out.println("Username is set to: " + playerName);
+                return;
+            } else{
+            System.out.println("Username is not valid!");
+            return null;
+            }
+    }while(true);
+    }
+
+}
