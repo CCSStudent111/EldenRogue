0b9847e5300dc91a6761a0cb88c8320048840d80
lilraffyel <rafael_calalang@dlsu.edu.ph>
2024-02-24 10:10:08 +0800

    Updated Char Creation
    Its pretty much done already for now since we dont have the Job Classes yet
    
    Just need to connect it also w the other classes
    
    then I think it should be good na
    
    feel free to modiy any of the code, juts lmk what u changed

--- CharacterCreation.java
+++ CharacterCreation.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
+
 public class CharacterCreation{
     private static String playerName;
     private static String playerClass;
+
     public static void main (String[] args){
-        Scanner sName = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int nInput;
 
         do{
@@ -13,48 +15,57 @@ public class CharacterCreation{
             System.out.println("3. Confirm");
             System.out.println("4. Back\n");
             nInput = scanner.nextInt();
+            scanner.nextLine();
             if(nInput < 1 || nInput > 4){
                 System.out.println("Invalid Input!");
             }
 
             switch(nInput){
             case 1: 
-                inputname(playerName);
+                inputName(scanner);
                 break;
             case 2:
                 // Getter Methods + if statement? //
                 break;
             case 3:
-                // add if statements for name and job class //
-                System.out.println("Are you sure you want to proceed?\n");
-                System.out.println("1. Yes");
-                System.out.println("2. No");
-                nInput = scanner.nextInt();
-    
-                if(nInput == 1){
-                    //Proceed to Game Lobby//
-                    } else if(nInput == 2){ 
-                    break;
-                    } else{
-                    System.out.println("Invalid Input!");
-                    break;
-                    } 
+                handleConfirmation(scanner);
+
                 }
         }while(nInput != 4);
+
+        scanner.close();
     }
 
-private static String inputName(Scanner sName){
+private static void inputName(Scanner scanner){
     do{
         System.out.println("Enter your username (25 character limit):\n");
-        playerName = sName.nextLine();
+        playerName = scanner.nextLine();
             if(playerName.length() != 0 && playerName.length() <= 25){
                 System.out.println("Username is set to: " + playerName);
-                return;
+                break;
             } else{
             System.out.println("Username is not valid!");
-            return null;
             }
     }while(true);
     }
+
+private static void handleConfirmation(Scanner scanner){
+    int nConfirm;
+    do{
+        System.out.println("Are you sure you want to proceed?\n");
+        System.out.println("1. Yes");
+        System.out.println("2. No");
+        
+        nConfirm = scanner.nextInt();
+            if(nConfirm == 1){
+            //Proceed to Game Lobby//
+            } else if(nConfirm == 2){ 
+                break;
+            } else{
+                System.out.println("Invalid Input!");
+            } 
+    }while(true);
+    }
+
 
 }
