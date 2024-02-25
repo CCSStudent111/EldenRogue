93a741dc571fb71e247df9d6f65c3ae0c49b5033
lilraffyel <rafael_calalang@dlsu.edu.ph>
2024-02-25 13:11:17 +0800

    Connecting JobClass to Statistics
    Attempting to implement getters and setters for Statistics
    
    Will also try to get started on Character(user) class later on

--- JobClass.java
+++ JobClass.java
@@ -1,8 +1,9 @@
 public class JobClass{
     private String className;
 
-    public JobClass(String className, int nHP, int nDEX, int nINT, int nEND, int nSTR, int nFTH){
+    JobClass(String className, int nLevel, int nHP, int nDEX, int nINT, int nEND, int nSTR, int nFTH){
         this.className = className;
+        this.nLevel = nLevel;
         this.nHP = nHP;
         this.nDEX = nDEX;
         this.nINT = nINT;
@@ -12,12 +13,12 @@ public class JobClass{
     }
 
     public static void main(String[] args){
-        JobClass Vagabond = new JobClass("Vagabond", 15, 13, 9, 11, 14, 9);
-        JobClass Samurai = new JobClass("Samurai", 12, 15, 9, 13, 12, 8);
-        JobClass Warrior = new JobClass("Warrior", 11, 16, 10, 11, 10, 8);
-        JobClass Hero = new JobClass("Hero", 14, 9, 7, 12, 16, 8);
-        JobClass Astrologer = new JobClass("Astrologer", 9, 12, 16, 9, 8, 7);
-        JobClass Prophet = new JobClass("Prophet", 10, 10, 7, 8, 11, 16);
+        JobClass Vagabond = new JobClass("Vagabond", 9, 15, 13, 9, 11, 14, 9);
+        JobClass Samurai = new JobClass("Samurai", 9, 12, 15, 9, 13, 12, 8);
+        JobClass Warrior = new JobClass("Warrior", 8, 11, 16, 10, 11, 10, 8);
+        JobClass Hero = new JobClass("Hero", 7, 14, 9, 7, 12, 16, 8);
+        JobClass Astrologer = new JobClass("Astrologer", 6, 9, 12, 16, 9, 8, 7);
+        JobClass Prophet = new JobClass("Prophet", 7, 10, 10, 7, 8, 11, 16);
 
     }
 
--- Statistics.java
+++ Statistics.java
@@ -1,4 +1,5 @@
 public class Statistics{
+    private int nLevel;
     private int nHP;
     private int nDEX;
     private int nINT;
@@ -6,4 +7,45 @@ public class Statistics{
     private int nSTR;
     private int nFTH;
     
+   Statistics (int nLevel, int nHP, int nDEX, int nINT, int nEND, int nSTR, int nFTH){
+    this.nLevel = nLevel;
+    this.nHP = nHP;
+    this.nDEX = nDEX;
+    this.nINT = nINT;
+    this.nEND = nEND;
+    this.nSTR = nSTR;
+    this.nFTH = nFTH;
 }
+
+    public int getLevel(){
+        return nLevel;
+    }
+
+    public int getHP(){
+        return nHP;
+    }
+
+    public int getDEX(){
+        return nDEX;
+    }
+
+    public int getINT(){
+        return nINT;
+    }
+
+    public int getEND(){
+        return nEND;
+    }
+
+    public int getSTR(){
+        return nSTR;
+    }
+
+    public int getFTH(){
+        return nFTH;
+    }
+
+
+
+}
+
